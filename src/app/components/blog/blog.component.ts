d1f6a7934a11fbbf12337e3b98d5884b420b6caf
Juan Antonio Pérez Jarillo <jantonio.perez@estudioclandestino.es>
2025-02-25 20:11:34 +0100

    criterio 5 finish

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { IPost } from '../../interfaces/ipost.interfaces';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
@@ -12,15 +13,15 @@ export class BlogComponent {
     {
       id: 1,
       title: 'First Post',
-      url: 'http://example.com/first-post',
+      url: 'https://www.grecia.info/es/wp-content/uploads/sites/49/creta-grecia-thumbnail.jpg',
       author: 'Author One',
       date: '2023-01-01',
-      body: 'This is the body of the first post.'
+      body: 'This <strong>is the body</strong> of the first post.'
     },
     {
       id: 2,
       title: 'Second Post',
-      url: 'http://example.com/second-post',
+      url: 'https://www.grecia.info/es/wp-content/uploads/sites/49/creta-grecia-thumbnail.jpg',
       author: 'Author Two',
       date: '2023-02-01',
       body: 'This is the body of the second post.'
@@ -28,11 +29,33 @@ export class BlogComponent {
     {
       id: 3,
       title: 'Third Post',
-      url: 'http://example.com/third-post',
+      url: 'https://www.grecia.info/es/wp-content/uploads/sites/49/creta-grecia-thumbnail.jpg',
       author: 'Author Three',
       date: '2023-03-01',
       body: 'This is the body of the third post.'
     }
   ];
+
+  ngOnInit() {
+    //this.getData();
+  }
+
+  /*   getData(): string {
+      let html = ""
+      this.arrPosts.forEach(post => {
+        html += `<article>
+        <figure>
+          <img src="${post.url}" alt="${post.title}">
+        </figure>
+        <h3>${post.title}</h3>
+        <p>Autor: ${post.author}</p>
+        <p>Fecha: ${post.date}</p>
+        <div class="content">
+          <p>${post.body}</p>
+        </div>
+      </article>`
+      })
+      return html;
+    } */
 
 }
